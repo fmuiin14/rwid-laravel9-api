aae5d64c36cab63f64e8ac0b1393edf763ae229a
fmuiin14 <fmuiin14@gmail.com>
2022-10-28 20:54:26 +0700

    2. bs view dan update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -39,5 +40,43 @@ class PostController extends Controller
         ]);
 
         return new PostResource(true, 'Data Post Berhasil Ditambahkan', $post);
+    }
+
+    public function show (Post $post)
+    {
+        return new PostResource(true, 'Data Post Ditemukan', $post);
+    }
+
+    public function update (Request $request, Post $post)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if ($request->hasFile('image')) {
+            $image_path = $request->file('image')->store('posts', 'public');
+
+            //delete old image
+            Storage::delete('public/'.$post->image);
+
+            $post->update([
+                'image'     => $image_path,
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        } else {
+            $post->update([
+                'title'     => $request->title,
+                'content'   => $request->content,
+            ]);
+        }
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
     }
 }
